2887b5350e1063705c460ae1784614af8ad79cc3
Aashishkumar123 <aashishkumar12376@gmail.com>
2021-05-22 20:48:44 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,14 @@
+from re import I
+from django.shortcuts import render
+import random
+import pyautogui
+from django.conf import settings
+from django.contrib import messages
+def index(request):
+    if request.method == "POST":
+        ss = pyautogui.screenshot()
+        img = f'myimg{random.randint(1000,9999)}.png'
+        ss.save(settings.MEDIA_ROOT/img)
+        messages.success(request,'screenshot has been taken')
+        return render(request,'index.html',{'img':img})
+    return render(request,'index.html')
